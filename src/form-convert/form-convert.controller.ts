f6331e45a80c81e7c4009fd80dabae5acf2dc135
Esteban Ginocchio Silva <comsat61@gmail.com>
2021-10-20 14:55:32 -0700

    handle HTTP request better

--- src/form-convert/form-convert.controller.ts
+++ src/form-convert/form-convert.controller.ts
@@ -7,16 +7,22 @@ export class FormConvertController {
   constructor(private configs: ConfigService, private http: HttpService) {}
 
   @Post()
-  async getHello(@Body() submission: FormDataCovidSafetyDto): Promise<any> {
-    const submissionAsJson = JSON.stringify(submission);
-    const deepCopy = JSON.parse(submissionAsJson);
-    Logger.verbose(submissionAsJson);
+  getHello(@Body() submission: any): any {
+    console.log(submission);
 
     const triggerUrl = this.configs.get('REPOST_TO_TRIGGER_URL');
-    return this.http.post(triggerUrl, deepCopy, {
+
+    const response = this.http.post(triggerUrl, submission, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
+
+    let result;
+    response.toPromise().then((res) => {
+      result = res;
+    });
+    console.log(result);
+    return result;
   }
 }
