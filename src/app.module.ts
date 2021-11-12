1eb5a2a77f779e425bd5d26b0089ea356acf42dd
Esteban Ginocchio Silva <comsat61@gmail.com>
2021-11-12 13:42:22 -0800

    remove unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FormConvertController } from './form-convert/form-convert.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), HttpModule],
-  controllers: [AppController, FormConvertController],
-  providers: [AppService],
+  controllers: [FormConvertController],
+  providers: [],
 })
 export class AppModule {}
