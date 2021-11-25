8ca5b53d7086f4a148d368eb89483582b1fdd32c
Esteban Ginocchio Silva <comsat61@gmail.com>
2021-11-24 17:07:38 -0800

    submodule and health checl
    signed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { FormAdaptorModule } from './form-adapter/form-adapter.module';
 import { FormConvertService } from './form-convert/form-convert.service';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigurationsModule } from './configurations/configurations.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigurationsModule } from './configurations/configurations.module';
     FormAdaptorModule,
     HttpModule,
     ConfigurationsModule,
+    HealthModule,
   ],
   controllers: [FormConvertController],
   providers: [FormConvertService],
---
+++ src/health/health.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  HttpHealthIndicator,
+  HealthCheck,
+  HealthCheckService,
+} from '@nestjs/terminus';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private configs: ConfigService,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () =>
+        this.http.pingCheck(
+          'app ping',
+          `${this.configs.get('APP_URL_SELF')}/health/ping`,
+        ),
+    ]);
+  }
+
+  @Get('ping')
+  ping() {
+    return 'pong';
+  }
+}
